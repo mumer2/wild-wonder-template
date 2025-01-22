079fd557e08eb5c0797bc0a6d0da54a865610365
mumer2 <umermalik8280@gmail.com>
2025-01-22 20:21:22 +0500

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Header.css';
+export default function Header() {
+  return (
+    <div className="header">
+      <div>
+        <h1 className='text-white text-center font-[600] text-[20px]'>FREE SHIPPING ON ORDERS $50+</h1>
+      </div>
+    </div>
+  )
+}
